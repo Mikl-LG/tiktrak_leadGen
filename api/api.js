a874d12f41cde565f08c7087ac532d398cf339a7
Mikl-LG <legallicmickael@inspekt.fr>
2021-02-15 10:14:52 +0100

    second commit

--- api/api.js
+++ api/api.js
@@ -6,14 +6,12 @@ api = {
         const natures = await database.natures.get({
             body : body
         })
-        natures && console.log('Natures sended to user');
         return(natures);
     },
     getNaturesFeatures : async(body) => {
         const naturesFeatures = await database.naturesFeatures.get({
             body : body
         })
-        naturesFeatures && console.log('Natures features sended to user');
         return(naturesFeatures);
     } 
 }
