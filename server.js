329a060fea90ff157752527d7b76916f0d52f9a9
Mikl-LG <legallicmickael@inspekt.fr>
2021-02-15 10:45:16 +0100

    fourth commit

--- server.js
+++ server.js
@@ -3,4 +3,4 @@ const app = require('./app');
 const port = process.env.PORT;
 
 
-app.listen(port, () => console.log(`TIK TRAK is running on port ${port}`));
+app.listen(port, () => console.log(`TIK TRAK is now running on port ${port}`));
